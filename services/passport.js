626fb8c39acd283b59d44016aca6cdd580bd1cf7
Prince K <k.prince1997@gmail.com>
2018-06-26 21:02:37 -0700

    modularize routes

---
+++ services/passport.js
@@ -0,0 +1,16 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('../config/keys');
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    accessToken => {
+      console.log(accessToken);
+    }
+  )
+);
