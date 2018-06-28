f21031f1388c0a02c2980801445fec72f79acd36
Prince K <k.prince1997@gmail.com>
2018-06-28 10:13:22 -0700

    added mlab and mongoose. Created a user schema.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const app = require('express')();
-require('../server/services/passport');
-require('../server/routes/authRoutes')(app);
+const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./services/passport');
+require('./models/User');
 
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+require('../server/routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 const keys = require('../config/keys');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -9,8 +12,8 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       callbackURL: '/auth/google/callback'
     },
-    accessToken => {
-      console.log(accessToken);
+    (accessToken, refreshToken, profile, done) => {
+        new User({googleId: profile.id});
     }
   )
 );
