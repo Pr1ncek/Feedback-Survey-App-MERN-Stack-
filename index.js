13c34dc976185fce94b1b2a112af41bec9cad394
Prince K <k.prince1997@gmail.com>
2018-07-03 08:03:39 -0700

    Finished Auth flow

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('../server/routes/authRoutes')(app);
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
